da024eba6323bd9e3303571bee6a9b46883efd6e
Dhruv Parekh <parekhdhruv04@gmail.com>
2022-05-23 16:48:22 +0530

    Rename files

--- pages/home.js
+++
@@ -1,8 +0,0 @@
-export const Home = () => {
-  return (
-    <div >
-      <p>Home</p>
-    </div>
-  )
-}
-export default Home
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
-export default function Index({ time }) {
+export const Home = () => {
   return (
-    <main>
-      <h1>SSR Caching with Next.js</h1>
-      <time dateTime={time}>{time}</time>
-    </main>
+    <div >
+      <p>Home</p>
+    </div>
   )
 }
-
-export async function getServerSideProps({ req, res }) {
-  // res.setHeader(
-  //   'Cache-Control',
-  //   'public, s-maxage=10, stale-while-revalidate=59'
-  // )
-
-  return {
-    props: {
-      time: new Date().toISOString(),
-    },
-  }
-}
+export default Home
