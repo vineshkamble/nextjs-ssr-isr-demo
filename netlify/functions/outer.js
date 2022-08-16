cc5342591114d2bdca5f216ea610c86e5ad5497a
dhruvparekh12 <51786302+dhruvparekh12@users.noreply.github.com>
2022-08-16 14:48:02 +0530

    prep for demo

--- netlify/functions/outer.js
+++
@@ -1,7 +0,0 @@
-module.exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: "Hello World from lambda(Without container).",
-      headers: { "Content-Type": "text/html" },
-    };
-  };
